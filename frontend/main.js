6a4a2c7fa7629468d9ac5417e93665ea9ff586ff
Thomas Bliem <tbliem@gmx.de>
2020-03-24 10:48:21 +0100

    can record to in-memory array of blobs

--- frontend/main.js
+++ frontend/main.js
@@ -1,9 +1,31 @@
-function start()
-{
-  console.log("start")
+var stream, recorder, chunks;
+
+function startStream() {
+  if (stream) {console.log("Stream already started."); return;}
+
+  console.log("Starting stream...");
+  navigator.mediaDevices.getUserMedia({video: false, audio: true})
+  .then(function(new_stream) {
+    stream = new_stream;
+    console.log("Stream started.");
+  })
+  .catch(function(err) {
+    console.log(err);
+  })
 }
 
-function stop()
+function startRecording()
 {
-  console.log("stop")
+  if (!stream)  {console.log("Steam not yet started.");    return;}
+  if (recorder) {console.log("Recorder already started."); return;}
+
+  recorder = new MediaRecorder(stream);
+  chunks = [];
+  recorder.ondataavailable = function(e) {chunks.push(e.data)}
+  recorder.start()
+}
+
+function stopRecording()
+{
+  recorder.stop();
 }
