a9da69e4e1461254c502a9656c05b44a8e6f0e26
Thomas Bliem <tbliem@gmx.de>
2020-03-24 13:42:58 +0100

    refactoring

--- frontend/main.js
+++ frontend/main.js
@@ -1,46 +1,52 @@
-var stream, recorder, chunks;
+var recorder, chunks;
 
-function startStream() {
-  if (stream) {console.log("Stream already started."); return;}
+document.addEventListener("DOMContentLoaded", initDocument);
 
-  console.log("Starting stream...");
-  navigator.mediaDevices.getUserMedia({video: false, audio: true})
-  .then(function(new_stream) {
-    stream = new_stream;
-    console.log("Stream started.");
-  })
-  .catch(function(err) {
-    console.log(err);
-  })
+function initDocument()
+{
+  console.log("Adding event handlers to DOM.")
+  document.getElementById("startStreamButton")  .onclick = startStream;
+  document.getElementById("stopRecordingButton").onclick = stopRecording;
 }
 
-function startRecording()
+function startStream()
 {
-  if (!stream)  {console.log("Steam not yet started.");    return;}
-  if (recorder) {console.log("Recorder already started."); return;}
+  console.log("Starting stream.");
+  navigator.mediaDevices.getUserMedia({audio: true}).then(startRecording).catch(console.log);
+}
 
+function startRecording(stream)
+{
+  console.log("Starting recording.")
   recorder = new MediaRecorder(stream);
   chunks = [];
-  recorder.ondataavailable = function(e) {chunks.push(e.data)}
-  recorder.start()
+  recorder.ondataavailable = pushChunk;
+  recorder.onstop = combineChunks;
+  recorder.start();
+}
+
+function pushChunk(event)
+{
+  console.log("Pushing chunk.");
+  chunks.push(event.data);
 }
 
 function stopRecording()
 {
-  recorder.onstop = saveBlob;
   recorder.stop();
 }
 
-function saveBlob()
+function combineChunks()
 {
+  console.log("Combining chunks.");
   var downloadButton;
 
   if(!chunks)            {console.log("Chunks not initialized"); return;}
   if(chunks.lenght == 0) {console.log("No chunks recorded");     return;}
   
-  blob = new Blob(chunks, {type: chunks[0].type})
+  blob = new Blob(chunks, {type: chunks[0].type});
   //chunks = undefined;  Does Blob() create a copy?
   downloadButton = document.getElementById("downloadButton");
-  downloadButton.href = URL.createObjectURL(blob)
-  downloadButton.download = "recording.oga"
+  downloadButton.href = URL.createObjectURL(blob);
+  downloadButton.download = "recording.oga";
 }
