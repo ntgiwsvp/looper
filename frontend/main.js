cebc292ad42c595228597fea950c7ceabf5f0df8
Thomas Bliem <tbliem@gmx.de>
2020-03-24 11:18:08 +0100

    can save audio to .oga file

--- frontend/main.js
+++ frontend/main.js
@@ -27,5 +27,20 @@ function startRecording()
 
 function stopRecording()
 {
+  recorder.onstop = saveBlob;
   recorder.stop();
+}
+
+function saveBlob()
+{
+  var downloadButton;
+
+  if(!chunks)            {console.log("Chunks not initialized"); return;}
+  if(chunks.lenght == 0) {console.log("No chunks recorded");     return;}
+  
+  blob = new Blob(chunks, {type: chunks[0].type})
+  //chunks = undefined;  Does Blob() create a copy?
+  downloadButton = document.getElementById("downloadButton");
+  downloadButton.href = URL.createObjectURL(blob)
+  downloadButton.download = "recording.oga"
 }
