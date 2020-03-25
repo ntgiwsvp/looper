df1575030c83b6faf2102031ffc15475a51d3851
Thomas Bliem <tbliem@gmx.de>
2020-03-25 19:37:31 +0100

    transmission client to server via signalserver ok

--- frontend/main.js
+++ frontend/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var recorder, chunks, connection, remoteConnection;
+var signalingChannel, connection;
 
 document.addEventListener("DOMContentLoaded", initDocument);
 
@@ -8,31 +8,36 @@ document.addEventListener("DOMContentLoaded", initDocument);
 function initDocument()
 {
   console.log("Adding event handlers to DOM.")
-  document.getElementById("startStreamButton")  .onclick = startStream;
-  document.getElementById("stopRecordingButton").onclick = stopRecording;
+  document.getElementById("startButton").onclick = startStream;
 }
 
 // Once the start button is hit, the first step is to get access to the
 // user's microphone.
 function startStream()
 {
+  console.log("Creating connection to signaling server.");
+  signalingChannel = new WebSocket("ws://localhost:8080/");
+  signalingChannel.addEventListener("message", receiveMessage);
+  // XXX Dirty trick that needs to be corrected:  Time to setup WebSocket
+  //     is hidden while user is approving media acces.  Should use
+  //     WebSocket.onopen to make sure not to send messages too early.
+
   console.log("Getting user media.");
   navigator.mediaDevices
     .getUserMedia({audio: true})
     .then(startCall)
-    .catch((err) => console.error(err));
 }
 
 // Once we obtained the stream from the user's microphone, we create the
 // RTC connection, register some event handlers, associate the stream,
 // and create an offer.
-// THIS FUNCTION ALSO CONTAINS SOME CODE TO BE RUN ON THE REMOTE SIDE
 function startCall(stream)
 {
-  console.log("Creating connection.")
+  console.log("Creating RTC connection.")
   connection = new RTCPeerConnection();
-
   connection.addEventListener('icecandidate', sendIceCandidate);
+  connection.addEventListener("connectionstatechange",
+    reportConnectionState);
 
   console.log("Adding stream to connection.");
   connection.addStream(stream);
@@ -40,126 +45,64 @@ function startCall(stream)
   console.log("Creating offer.")
   connection
     .createOffer({voiceActivityDetection: false})
-    .then(setLocalDescription)
+    .then(sendOffer)
     .catch((err) => console.error(err));
-
-  // REMOTE CONNECTION IS FAKE.  THIS WILL BE ON THE SERVER
-
-  console.log("REMOTE: Creating connection.");
-  remoteConnection = new RTCPeerConnection();
-
-  remoteConnection.addEventListener('icecandidate', handleRemoteConnection);
-  remoteConnection.addEventListener('addstream', gotRemoteMediaStream);
 }
 
-// Once the offer has been created, we set the local description.
-// THIS FUNCTION ALSO CONTAINS SOME CODE TO BE RUN ON THE REMOTE SIDE
-function setLocalDescription(description)
+function receiveMessage(event)
 {
+  var data;
+
+  data = JSON.parse(event.data);
+
+  if (data.answer)
+  {
+    console.log("Received answer.")
+
+    console.log("Setting remote description.")
+    connection
+      .setRemoteDescription(new RTCSessionDescription(data.answer))
+      .then(() => console.log("Remote description set."))
+      .catch((err) => console.error(err));
+  }
+
+  if (data.candidate)
+  {
+    console.log("Received ICE candidate.")
+
+    console.log("Adding ICE candidate to connection.")
+    connection
+      //.addIceCandidate(new RTCIceCandidate(event.candidate))
+      .addIceCandidate(data.iceCandidate)
+      .then(() => console.log("ICE candidate added to connection."))
+      .catch((err) => console.error(err))
+  }
+}
+
+function reportConnectionState(event)
+{
+  console.log("Connection state: %s.", connection.connectionState)
+}
+
+function sendOffer(description)
+{
+  console.log("Created offer.");
+
   console.log("Setting local description.");
   connection
     .setLocalDescription(description)
     .then(() => console.log("Local description set."))
     .catch((err) => console.error(err));
 
-  // REMOTE CONNECTION IS FAKE, JUST FOR TESTING PURPOSES.  THIS WILL BE ON THE SERVER
-
-  console.log("REMOTE: Setting remote description.");
-  remoteConnection
-    .setRemoteDescription(description)
-    .then(() => console.log("REMOTE: Remote description set."))
-    .catch((err) => console.error(err));
-
-  console.log("REMOTE: Creating answer.");
-  remoteConnection
-    .createAnswer()
-    .then(createdAnswer)
-    .catch((err) => console.error(err));
-}
-
-// This is to be run on the REMOTE side.  Once a stream is added to the
-// remote connection, recording starts.
-function gotRemoteMediaStream(event)
-{
-  console.log("REMOTE EVENT addstream")
-  startRecording(event.stream);
-}
-
-// This is to be run on the REMOTE side.  Just a stream recorder.
-function startRecording(stream)
-{
-  console.log("Starting recording.")
-  recorder = new MediaRecorder(stream);
-  chunks = [];
-  recorder.ondataavailable = pushChunk;
-  recorder.onstop = combineChunks;
-  recorder.start();
+  console.log("Sending offer.");
+  signalingChannel.send(JSON.stringify({"offer": description}));
 }
 
 function sendIceCandidate(event)
 {
-  console.log("EVENT icecandidate()")
-
   if (event.candidate)
   {
-    remoteConnection  // FAKE
-      .addIceCandidate(new RTCIceCandidate(event.candidate))
-      .then(() => console.log("Connection successful."))
-      .catch(() => console.log("Connection failed"))
+    console.log("Sending ICE candidate to signaling server");
+    signalingChannel.send(JSON.stringify({"iceCandidate": event.candidate}));
   }
-}
-
-function createdAnswer(description)
-{
-  console.log("REMOTE: Setting local description.")
-  remoteConnection
-    .setLocalDescription(description)
-    .then(() => console.log("REMOTE: Local description set."))
-    .catch((err) => console.error(err));
-
-  // CONNECTIION IS FAKE, JUST FOR TESTING PURPOSES.  THIS WILL BE ON THE CLIENT
-
-  console.log("Setting remote description.")
-  connection
-    .setRemoteDescription(description)
-    .then(() => console.log("Remote description set."))
-    .catch((err) => console.error(err));
-}
-
-function handleRemoteConnection(event)
-{
-  console.log("REMOTE EVENT icecandidate()")
-
-  if (event.candidate)
-  {
-    connection  // FAKE
-      .addIceCandidate(new RTCIceCandidate(event.candidate))
-      .then(() => console.log("REMOTE Connection successful."))
-      .catch(() => console.log("REMOTE Connection failed"))
-  }
-}
-
-function pushChunk(event)
-{
-  console.log("Pushing chunk.");
-  chunks.push(event.data);
-}
-
-function stopRecording()
-{
-  recorder.stop();
-}
-
-function combineChunks()
-{
-  console.log("Combining chunks.");
-  var downloadButton, blob;
-
-  if(!chunks)            {console.log("Chunks not initialized"); return;}
-  if(chunks.lenght == 0) {console.log("No chunks recorded");     return;}
-  
-  blob = new Blob(chunks, {type: chunks[0].type});
-  downloadButton = document.getElementById("downloadButton");
-  downloadButton.href = URL.createObjectURL(blob);
-  downloadButton.download = "recording.oga";
 }
--- streamserver/main.js
+++ streamserver/main.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var socket;
+var signalingChannel, connection, recorder, chunks;
 
 document.addEventListener("DOMContentLoaded", initDocument);
 
@@ -8,11 +8,124 @@ function initDocument()
 {
   console.log("Adding event handlers to DOM.");
   document.getElementById("startServerButton").onclick = startServer;
+  document.getElementById("stopRecordingButton").onclick = stopRecording;
 }
 
 function startServer()
 {
+  console.log("Creating RTC connection");
+  connection = new RTCPeerConnection();
+  connection.addEventListener('icecandidate', sendIceCandidate);
+  connection.addEventListener('addstream', gotRemoteMediaStream);
+  connection.addEventListener("connectionstatechange",
+    reportConnectionState);
+  
   console.log("Creating connection to signaling server.");
-  socket = new WebSocket("ws://localhost:8080/")
+  signalingChannel = new WebSocket("ws://localhost:8080/")
+  signalingChannel.addEventListener("message", receiveMessage);
+
+  console.log("Waiting for offers.")
 }
 
+function reportConnectionState(event)
+{
+  console.log("Connection state: %s.", connection.connectionState)
+}
+
+function receiveMessage(event)
+{
+  var data;
+
+  data = JSON.parse(event.data);
+
+  if (data.offer)
+  {
+    console.log("Received offer.")
+
+    console.log("Setting remote description.");
+    connection
+      .setRemoteDescription(new RTCSessionDescription(data.offer))
+      .then(() => console.log("Remote description set."))
+      .catch((err) => console.error(err));
+  
+    console.log("Creating answer.");
+    connection
+      .createAnswer()
+      .then(sendAnswer)
+      .catch((err) => console.error(err));
+  }
+
+  if (data.iceCandidate)
+  {
+    console.log("Received ICE candidate.")
+
+    console.log("Adding ICE candidate to connection.")
+    connection.addIceCandidate(data.iceCandidate)
+      .then(() => console.log("ICE candidate added to connection."))
+      .catch((err) => console.error(err))
+  }
+}
+
+function sendAnswer(description)
+{
+  console.log("Created answer.");
+
+  console.log("Setting local description.")
+  connection
+    .setLocalDescription(description)
+    .then(() => console.log("Local description set."))
+    .catch((err) => console.error(err));
+
+  console.log("Sending answer.")
+  signalingChannel.send(JSON.stringify({"answer": description}));
+}
+
+function sendIceCandidate(event)
+{
+  if (event.candidate)
+  {
+    console.log("Sending ICE candidate to signaling server");
+    signalingChannel.send(JSON.stringify({"iceCandidate": event.candidate}));
+  }
+}
+
+function gotRemoteMediaStream(event)
+{
+  console.log("Got remote media stream.")
+  startRecording(event.stream);
+}
+
+function startRecording(stream)
+{
+  console.log("Starting recording.")
+  recorder = new MediaRecorder(stream);
+  chunks = [];
+  recorder.ondataavailable = pushChunk;
+  recorder.onstop = combineChunks;
+  recorder.start();
+}
+
+function pushChunk(event)
+{
+  console.log("Pushing chunk.");
+  chunks.push(event.data);
+}
+
+function stopRecording()
+{
+  recorder.stop();
+}
+
+function combineChunks()
+{
+  console.log("Combining chunks.");
+  var downloadButton, blob;
+
+  if(!chunks)            {console.log("Chunks not initialized"); return;}
+  if(chunks.lenght == 0) {console.log("No chunks recorded");     return;}
+  
+  blob = new Blob(chunks, {type: chunks[0].type});
+  downloadButton = document.getElementById("downloadButton");
+  downloadButton.href = URL.createObjectURL(blob);
+  downloadButton.download = "recording.oga";
+}
