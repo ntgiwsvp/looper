01b95d19dce37dd3cecf62ced2efbe4893fd883b
Thomas Bliem <tbliem@gmx.de>
2020-03-29 17:45:20 +0200

    simultaneous phase detection for 13 frequencies

--- scratchpad/latency.js
+++ scratchpad/latency.js
@@ -16,13 +16,20 @@ var f, f_s, R, analyser0, analyser90, dataArray0, dataArray90;
 
 async function start()
 {
-  var mediaStream, audioContext, track, i;
-  var tmp, REF_0deg, REF_90deg, X, Y0, Y90, Y0F, Y90F;
+  var mediaStream, audioContext, track;
+  var tmp, REF_0deg, REF_90deg, X, Y0, Y90, Y0F, Y90F, i;
+
+  const test = true;
 
   audioContext = new AudioContext();
 
-  f = 1000; // frequency of reference signal
-  R = 1.0;    // amplitude of reference signal
+  f = [1000, 1100, 1200, 1300, 1400,
+       1500, 1600, 1700, 1800, 1900,
+       2000, 2100, 2200];
+  R = [0.0768, 0.0768, 0.0768, 0.0768, 0.0768,
+       0.0768, 0.0768, 0.0768, 0.0768, 0.0768,
+       0.0768, 0.0768, 0.0768]; 
+  
   f_s = audioContext.sampleRate;
   const alpha = 0.99999;
   const feedforward = [1 - alpha];
@@ -37,73 +44,77 @@ async function start()
   // https://www.fmf.uni-lj.si/~ponikvar/PDFji/Phase%20angle%20measurements.pdf
   // with similar content, not read this one yet.)
 
-  tmp       = new OscillatorNode(audioContext, {frequency: f});           
-  REF_0deg  = new GainNode      (audioContext, {gain:      R});
-  REF_90deg = new DelayNode     (audioContext, {delayTime: 0.25/f});
-  X         = new DelayNode     (audioContext, {delayTime: 0.123/f});
-  Y0        = new GainNode      (audioContext, {gain:      0});
-  Y90       = new GainNode      (audioContext, {gain:      0});
-  Y0F       = new IIRFilterNode (audioContext, {feedforward, feedback});
-  Y90F      = new IIRFilterNode (audioContext, {feedforward, feedback});
+  tmp       = [];
+  REF_0deg  = [];
+  REF_90deg = [];
+  Y0        = [];
+  Y90       = [];
+  Y0F       = [];
+  Y90F      = [];
+  analyser0 = [];
+  analyser90 = [];
 
-  if(true)
+  mediaStream =  await navigator.mediaDevices.getUserMedia({audio: {
+    echoCancellation: false,
+    noiseSuppression: false,
+    channelCount:     1}});
+  console.log(mediaStream);
+  track = mediaStream.getAudioTracks()[0];
+  X = new MediaStreamAudioSourceNode(audioContext, {mediaStream});
+
+  if (test) X = new DelayNode(audioContext, {delayTime: 5/f_s});
+  
+  for (i = 0; i < 13; i++)
   {
-    mediaStream =  await navigator.mediaDevices.getUserMedia({audio: {
-      echoCancellation: false,
-      noiseSuppression: false,
-      channelCount:     1}});
-    console.log(mediaStream);
-    track = mediaStream.getAudioTracks()[0];
+    tmp      [i] = new OscillatorNode(audioContext, {frequency: f[i]});           
+    REF_0deg [i] = new GainNode      (audioContext, {gain:      R[i]});
+    REF_90deg[i] = new DelayNode     (audioContext, {delayTime: 0.25/f[i]});
+    Y0       [i] = new GainNode      (audioContext, {gain:      0});
+    Y90      [i] = new GainNode      (audioContext, {gain:      0});
+    Y0F      [i] = new IIRFilterNode (audioContext, {feedforward, feedback});
+    Y90F     [i] = new IIRFilterNode (audioContext, {feedforward, feedback});
 
-    X = new MediaStreamAudioSourceNode(audioContext, {mediaStream});
-    REF_0deg .connect(audioContext.destination);
+    REF_0deg [i].connect(audioContext.destination);
+    tmp      [i].connect(REF_0deg [i]);
+    REF_0deg [i].connect(REF_90deg[i]);
+    REF_0deg [i].connect(Y0       [i].gain);
+    REF_90deg[i].connect(Y90      [i].gain);
+    X           .connect(Y0       [i]);
+    X           .connect(Y90      [i]);
+    Y0       [i].connect(Y0F      [i]);
+    Y90      [i].connect(Y90F     [i]);
+
+    tmp[i].start();
+
+    // Abuse analyser to get samples, as in the MDN oscilloscope examples.
+    analyser0 [i] = new AnalyserNode(audioContext, {fftSize: 32});
+    analyser90[i] = new AnalyserNode(audioContext, {fftSize: 32});
+    Y0F       [i].connect(analyser0[i]);
+    Y90F      [i].connect(analyser90[i]);
   }
-  else // test setup
-  {
-    X       = new DelayNode     (audioContext, {delayTime: 0.123/f});
-    REF_0deg.connect(X);    
-  }
-
-  tmp      .connect(REF_0deg);
-  REF_0deg .connect(REF_90deg);
-  REF_0deg .connect(Y0.gain);
-  REF_90deg.connect(Y90.gain);
-  X        .connect(Y0);
-  X        .connect(Y90);
-  Y0       .connect(Y0F);
-  Y90      .connect(Y90F);
-
-  if(true)
-  {
-
-  }
-  else // test setup
-  {
-
-  }
-
-  tmp.start();
-
-  // Abuse analyser to get samples, as in the MDN oscilloscope examples.
-  analyser0  = new AnalyserNode(audioContext, {fftSize: 32});
-  analyser90 = new AnalyserNode(audioContext, {fftSize: 32});
-  Y0F.connect(analyser0);
-  Y90F.connect(analyser90);
-  dataArray0 = new Float32Array(1);
+  
+  if (test) for (i = 0; i < 13; i++) REF_0deg[i].connect(X);
+  
+  dataArray0  = new Float32Array(1);
   dataArray90 = new Float32Array(1);
 }
 
 function print()
 {
-  var A, phi;
+  var A, phi, i;
 
-  analyser0.getFloatTimeDomainData(dataArray0);
-  analyser90.getFloatTimeDomainData(dataArray90);
+  console.log("---");
 
-  A = 2/R*Math.sqrt(dataArray0[0]**2 + dataArray90[0]**2);
-  phi = Math.atan2(dataArray90[0], dataArray0[0]);
-  
-  console.log("channel gain: %.3f, latency = %.1f samples (of %.1f)",
-    A/R, phi/(2*Math.PI)*f_s/f, f_s/f);
+  for (i = 0; i < 13; i++)
+  {
+    analyser0[i].getFloatTimeDomainData(dataArray0);
+    analyser90[i].getFloatTimeDomainData(dataArray90);
+
+    A = 2/R[i]*Math.sqrt(dataArray0[0]**2 + dataArray90[0]**2);
+    phi = Math.atan2(dataArray90[0], dataArray0[0]);
+    
+    console.log("%.0f Hz: channel gain: %.3f, latency = %.1f samples (of %.1f)",
+      f[i], A/R[i], phi/(2*Math.PI)*f_s/f[i], f_s/f[i]);
+  }
 }
 
