d34f90ff7e57a06b33378cfa00630957a7d17fbf
Thomas Bliem <tbliem@gmx.de>
2020-03-30 17:07:29 +0200

    refactoring

--- scratchpad/latency.js
+++ scratchpad/latency.js
@@ -1,7 +1,39 @@
 'use strict';
 
-var audioContext;
+// Set this to true to shortcut the channel and work with a fixed
+// latency instead.
+const test = false;
 
+
+// 13 test frequencies - using basically the same ones as jack-delay,
+// just there they are specified relative to the sample rate, here
+// we just set fixed values.  They correspond to jack-delay's values at
+// 48000 Hz.
+const f = [        3000,
+            1/   2*3000,
+            3/   4*3000,
+            5/   8*3000,
+            9/  16*3000,
+           17/  32*3000,
+           17/  64*3000,
+           41/ 128*3000,
+           97/ 256*3000,
+          225/ 512*3000,
+          833/1024*3000,
+         1793/2048*3000,
+         3841/4096*3000];
+
+// Amplitude of each of the 13 test frequencies.  For now just use 1/13 for
+// each.  Other options: Could put higher ones to the frequencies where
+// the channel is expected to have more loss.  Or, as jack-delay, boost f_0
+// on the expense of the other ones.
+const R = [0.0768, 0.0768, 0.0768, 0.0768, 0.0768,
+           0.0768, 0.0768, 0.0768, 0.0768, 0.0768,
+           0.0768, 0.0768, 0.0768];
+
+// For filter
+const alpha = 0.99999;
+         
 document.addEventListener("DOMContentLoaded", initDocument);
 
 // We start by associating the event handlers to the frontend.
@@ -12,40 +44,19 @@ function initDocument()
   document.getElementById("printButton").onclick = print;
 }
 
-var f, f_s, R, analyser, dataArray;
+var audioContext, f_s, dataArray;
+// f_s = sample rate should be a local variable to start eventually.
+// Currently just need it global to print the latency in no. of samples
+// in the print function.  Should anyway move to ms.
 
 async function start()
 {
-  var mediaStream, audioContext, track, merger;
-  var tmp, REF_0deg, REF_90deg, X, Y0, Y90, Y0F, Y90F, i;
-
-  const test = true;
+  var mediaStream, audioContext, track;
+  var tmp, REF_0deg, REF_90deg, X, Y0, Y90, Y0F, Y90F, i, analyser, merger;
 
   audioContext = new AudioContext();
 
-  f = [        3000,
-        1/   2*3000,
-        3/   4*3000,
-        5/   8*3000,
-        9/  16*3000,
-       17/  32*3000,
-       17/  64*3000,
-       41/ 128*3000,
-       97/ 256*3000,
-      225/ 512*3000,
-      833/1024*3000,
-     1793/2048*3000,
-     3841/4096*3000];
-
-  R = [0.0768, 0.0768, 0.0768, 0.0768, 0.0768,
-       0.0768, 0.0768, 0.0768, 0.0768, 0.0768,
-       0.0768, 0.0768, 0.0768]; 
-  
   f_s = audioContext.sampleRate;
-  const alpha = 0.99999;
-  const feedforward = [1 - alpha];
-  const feedback = [1, -alpha];
-
   console.log("Corner freqency: %.2f Hz.", -f_s*Math.log(alpha)/(2*Math.PI));
   
 
@@ -71,7 +82,7 @@ async function start()
   track = mediaStream.getAudioTracks()[0];
   X = new MediaStreamAudioSourceNode(audioContext, {mediaStream});
 
-  if (test) X = new DelayNode(audioContext, {delayTime: 5/f_s});
+  if (test) X = new DelayNode(audioContext, {delayTime: 10/f_s});
 
   merger = new ChannelMergerNode(audioContext, {numberOfInputs: 26});
 
@@ -83,32 +94,31 @@ async function start()
 
   for (i = 0; i < 13; i++)
   {
-    tmp      [i] = new OscillatorNode(audioContext, {frequency: f[i]});           
-    REF_0deg [i] = new GainNode      (audioContext, {gain:      R[i]});
-    REF_90deg[i] = new DelayNode     (audioContext, {delayTime: 0.25/f[i]});
-    Y0       [i] = new GainNode      (audioContext, {gain:      0});
-    Y90      [i] = new GainNode      (audioContext, {gain:      0});
-    Y0F      [i] = new IIRFilterNode (audioContext, {feedforward, feedback});
-    Y90F     [i] = new IIRFilterNode (audioContext, {feedforward, feedback});
+    tmp      [i] = new OscillatorNode(audioContext, {frequency:   f[i]       });           
+    REF_0deg [i] = new GainNode      (audioContext, {gain:        R[i]       });
+    REF_90deg[i] = new DelayNode     (audioContext, {delayTime:   0.25/f[i]  });
+    Y0       [i] = new GainNode      (audioContext, {gain:        0          });
+    Y90      [i] = new GainNode      (audioContext, {gain:        0          });
+    Y0F      [i] = new IIRFilterNode (audioContext, {feedforward: [1 - alpha],
+                                                     feedback:    [1, -alpha]});
+    Y90F     [i] = new IIRFilterNode (audioContext, {feedforward: [1 - alpha],
+                                                     feedback:    [1, -alpha]});
 
-    REF_0deg [i].connect(audioContext.destination);
-    tmp      [i].connect(REF_0deg [i]);
-    REF_0deg [i].connect(REF_90deg[i]);
-    REF_0deg [i].connect(Y0       [i].gain);
-    REF_90deg[i].connect(Y90      [i].gain);
-    X           .connect(Y0       [i]);
-    X           .connect(Y90      [i]);
-    Y0       [i].connect(Y0F      [i]);
-    Y90      [i].connect(Y90F     [i]);
+              REF_0deg [i].connect(audioContext.destination);
+    if (test) REF_0deg [i].connect(X                       );
+              tmp      [i].connect(REF_0deg [i]            );
+              REF_0deg [i].connect(REF_90deg[i]            );
+              REF_0deg [i].connect(Y0       [i].gain       );
+              REF_90deg[i].connect(Y90      [i].gain       );
+              X           .connect(Y0       [i]            );
+              X           .connect(Y90      [i]            );
+              Y0       [i].connect(Y0F      [i]            );
+              Y90      [i].connect(Y90F     [i]            );
+              Y0F      [i].connect(merger                  , 0,      i);
+              Y90F     [i].connect(merger                  , 0, 13 + i);
 
     tmp[i].start();
-
-    Y0F       [i].connect(merger, 0,      i);
-    Y90F      [i].connect(merger, 0, 13 + i);
   }
-  
-  if (test) for (i = 0; i < 13; i++) REF_0deg[i].connect(X);
-  
 }
 
 function savePhaseValues(event)
