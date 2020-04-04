e303a158d98f9a89e75076cbdb018569a892dca8
Thomas Bliem <tbliem@gmx.de>
2020-04-04 19:30:25 +0200

    refactoring

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -1,17 +1,16 @@
 "use strict";
 
-var https = require("https");
-var pem = require("pem");
-var express = require("express");
+const https   = require("https");
+const pem     = require("pem");
+const express = require("express");
 
+const app = express();
+app.use(express.static("../frontend"));
 pem.createCertificate({days: 1, selfSigned: true}, callback);
     
 function callback(err, keys)
 {
   if (err) throw err;
-
-  const app = express();
-  app.use(express.static("../frontend"));
 
   https
     .createServer({key: keys.serviceKey, cert: keys.certificate}, app)
