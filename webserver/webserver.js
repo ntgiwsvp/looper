133f77ca8d487f08a0e5825b6d3557b7adc9f83b
Thomas Bliem <tbliem@gmx.de>
2020-04-04 20:56:00 +0200

    improved ssl handling

--- webserver/webserver.js
+++ webserver/webserver.js
@@ -3,18 +3,23 @@
 const https   = require("https");
 const pem     = require("pem");
 const express = require("express");
+const fs      = require("fs");
 
 const app = express();
 app.use(express.static("../frontend"));
-pem.createCertificate({days: 1, selfSigned: true}, callback);
+pem.createCertificate({
+  days:               1,
+  commonName:         "127.0.0.1",
+  serviceCertificate: fs.readFileSync("ca.crt"),
+  serviceKey:         fs.readFileSync("ca.key")}, callback);
     
 function callback(err, keys)
 {
   if (err) throw err;
 
   https
-    .createServer({key: keys.serviceKey, cert: keys.certificate}, app)
+    .createServer({key: keys.clientKey, cert: keys.certificate}, app)
     .listen(4300);
 
-  console.log("Listening on https://localhost:4300");
+  console.log("Listening on https://127.0.0.1:4300");
 }
